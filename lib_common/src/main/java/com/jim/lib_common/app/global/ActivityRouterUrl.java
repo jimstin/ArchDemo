55f432c7857a9c97e747ae862c5eef52e077672d
Jim <jimstin@126.com>
2021-01-03 23:39:16 +0800

    统一ARouter url

---
+++ lib_common/src/main/java/com/jim/lib_common/app/global/ActivityRouterUrl.java
@@ -0,0 +1,16 @@
+package com.jim.lib_common.app.global;
+
+/**
+ * Activity路由Url
+ * 格式：ROUTE + moduleName + activityName
+ */
+public class ActivityRouterUrl {
+
+    //module_main
+    public static final String ROUTE_MODULE_MAIN_SPLASH_ACTIVITY="/module_main/SplashActivity";
+    public static final String ROUTE_MODULE_MAIN_MAIN_ACTIVITY="/module_main/MainActivity";
+
+    //module_news
+    public static final String ROUTE_MODULE_NEWS_NEWS_ACTIVITY="/module_main/NewsActivity";
+
+}
--- module_main/src/main/java/com/jim/module_main/MainActivity.kt
+++ module_main/src/main/java/com/jim/module_main/MainActivity.kt
@@ -5,14 +5,15 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
+import com.jim.lib_common.app.global.ActivityRouterUrl
 
-@Route(path = "/module_main/MainActivity")
+@Route(path = ActivityRouterUrl.ROUTE_MODULE_MAIN_MAIN_ACTIVITY)
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.module_main_activity_main)
         findViewById<TextView>(R.id.tv_main).setOnClickListener {
-            ARouter.getInstance().build("/module_news/NewsMainActivity").navigation()
+            ARouter.getInstance().build(ActivityRouterUrl.ROUTE_MODULE_NEWS_NEWS_ACTIVITY).navigation()
         }
     }
 }
